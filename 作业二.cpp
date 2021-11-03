17fa2cadce69e499a82fc2a2fda3e585eb4093fe
dedicatus543 <74846173+dedicatus543@users.noreply.github.com>
2021-11-03 15:26:07 +0800

    Add files via upload

---
+++ 作业二.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+#define N 300 //range of p
+int Isprime(float x);
+int main(){
+	
+	for(float p=3;p<=N;p++){
+		for(float q=2;p>q;q++){
+				int a=pow(p,q);
+				int b=pow(q,p);
+				if(Isprime(a+b) /*&& Isprime(q) && Isprime(p)*/)
+				printf("%g %g\n",p,q);
+			}
+		}
+	}
+
+
+int Isprime(float x){
+	int a=(int)x;
+	for(int i=2;i*i<=a;i++){
+		if(!(a%i))
+		return 0;
+	}
+	return 1;
+}
